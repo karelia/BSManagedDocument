01927d8e7119af2fd3454370023f11cb53087362
Abizer Nasir <abizern@abizern.org>
2013-01-26 11:54:13 +0000

    Add test targets for ARC and Non-ARC projects
    Normally, I would just use one common test class, but I'll leave them as
    individual ones for now to keep the tests more flexible.

---
+++ BSTestApps/BSTest/BSTestTests/BSTestTests.h
@@ -0,0 +1,13 @@
+//
+//  BSTestTests.h
+//  BSTestTests
+//
+//  Created by Abizer Nasir on 26/01/2013.
+//  Copyright (c) 2013 Jungle Candy Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BSTestTests : SenTestCase
+
+@end
