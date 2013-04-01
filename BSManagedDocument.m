26c6e5de3ba5fa3c3d6219ee3c3d9fe021425a02
Mike Abdullah <me@mikeabdullah.net>
2013-04-01 22:01:31 +0100

    Avoid deadlock should subclass's additional content writing access the MOC/PSC

--- BSManagedDocument.m
+++ BSManagedDocument.m
@@ -525,16 +525,17 @@ originalContentsURL:(NSURL *)originalContentsURL
 #endif
 
             _store = migrated;
-            
-            return [self writeAdditionalContent:_additionalContent
-                                          toURL:inURL
-                            originalContentsURL:originalContentsURL
-                                          error:error];
         }
         @finally
         {
             [coordinator unlock];
         }
+        
+        // MUST happen after unlocking the coordinator otherwise a subclass might try to access the context/coordinator resulting in deadlock
+        return [self writeAdditionalContent:_additionalContent
+                                      toURL:inURL
+                        originalContentsURL:originalContentsURL
+                                      error:error];
     }
     else if (saveOp != NSSaveOperation && saveOp != NSAutosaveInPlaceOperation)
     {
