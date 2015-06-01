9d1e43ad4df5acbb8ef08e155fa5e4d0e05f29b2
Mike Abdullah <me@mikeabdullah.net>
2015-06-01 20:12:57 +0200

    Remove override of -saveToURL:…
    It’s clients responsibility to use non-deprecated API instead

--- BSManagedDocument.m
+++ BSManagedDocument.m
@@ -533,19 +533,6 @@
     }];
 }
 
-- (BOOL)saveToURL:(NSURL *)url ofType:(NSString *)typeName forSaveOperation:(NSSaveOperationType)saveOperation error:(NSError **)outError;
-{
-    BOOL result = [super saveToURL:url ofType:typeName forSaveOperation:saveOperation error:outError];
-    
-    if (result &&
-        (saveOperation == NSSaveOperation || saveOperation == NSAutosaveInPlaceOperation || saveOperation == NSSaveAsOperation))
-    {
-        [self deleteAutosavedContentsTempDirectory];
-    }
-    
-    return result;
-}
-
 /*	Regular Save operations can write directly to the existing document since Core Data provides atomicity for us
  */
 - (BOOL)writeSafelyToURL:(NSURL *)absoluteURL
